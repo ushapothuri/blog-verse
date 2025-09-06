c8231fe24c85266ce127019d463df046535bb53d
ushapothuri <ushasusmithapothuri@gmail.com>
2025-09-06 12:35:24 +0530

    asadf

--- sample.js
+++ sample.js
@@ -1,1 +1,39 @@
 console.log("there's nothing")
+
+let name = "USHA";
+console.log(name)
+
+let isLogged=false
+let num = 100
+
+console.log(name)
+
+console.log(typeof num)
+console.log(typeof name)
+console.log(typeof isLogged)
+
+let x;
+console.log(x)
+console.log(typeof x)
+
+let y=null;
+
+console.log(y)
+console.log(typeof y)
+
+
+let arr=[1,2,3,4,5]
+console.log(arr[1])
+
+
+let person =
+{
+    name : "USHA",
+    age : 17
+}
+
+console.log(person.name)
+console.log(typeof person.name)
+
+console.log(person.age)
+console.log(typeof person.age)
